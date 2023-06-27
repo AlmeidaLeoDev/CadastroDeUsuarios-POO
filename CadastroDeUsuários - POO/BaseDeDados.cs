f5e591350f0441775d6ecf750b8433a83d57c4d2
AlmeidaLeoDev <almeidaleo.dev@gmail.com>
2023-06-27 15:42:04 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroDeUsuários - POO/BaseDeDados.cs
@@ -0,0 +1,49 @@
+﻿using CadastroUsuario;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroUsuario
+{
+    internal class BaseDeDados
+    {
+        //List
+        List<CadastroPessoa> listaDePessoas;
+
+        //Métodos
+        public void AdicionarPessoas(CadastroPessoa pPessoa)
+        {
+            listaDePessoas.Add(pPessoa);
+        }
+        public List<CadastroPessoa> PesquisaPessoasPorDoc(string pNumeroDeDocumento)
+        {
+            List<CadastroPessoa> listaDePessoasTemp = listaDePessoas.Where(x => x.NumeroDoDocumento == pNumeroDeDocumento).ToList();
+            if (listaDePessoasTemp.Count > 0)
+                return listaDePessoasTemp;
+            else
+                return null;
+        }
+        public List<CadastroPessoa> RemoverPessoasPorDoc(string pNumeroDeDocumento)
+        {
+            List<CadastroPessoa> listaDePessoasTemp = listaDePessoas.Where(x => x.NumeroDoDocumento == pNumeroDeDocumento).ToList();
+            if (listaDePessoasTemp.Count > 0)
+            {
+                foreach (CadastroPessoa pessoa in listaDePessoasTemp)
+                {
+                    listaDePessoas.Remove(pessoa);
+                }
+                return listaDePessoasTemp;
+            }
+            else
+                return null;
+        }
+
+        //Construtor
+        public BaseDeDados()
+        {
+            listaDePessoas = new List<CadastroPessoa>();
+        }
+    }
+}
---
+++ CadastroDeUsuários - POO/CadastroPessoa.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroUsuario
+{
+    internal class CadastroPessoa
+    {
+        //Propriedades
+        private string nome;
+        private string numeroDoDocumento;
+        private DateTime dataDeNascimento;
+        private string nomeDaRua;
+        private UInt32 numeroDaCasa;
+
+        //Atributos
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        public string NumeroDoDocumento
+        {
+            get { return numeroDoDocumento; }
+            set { numeroDoDocumento = value; }
+        }
+        public DateTime DataDeNascimento
+        {
+            get { return dataDeNascimento; }
+            set { dataDeNascimento = value; }
+        }
+        public string NomeDaRua
+        {
+            get { return nomeDaRua; }
+            set { nomeDaRua = value; }
+        }
+        public UInt32 NumeroDaCasa
+        {
+            get { return numeroDaCasa; }
+            set { numeroDaCasa = value; }
+        }
+
+        //Método construtor
+        public CadastroPessoa(string nome, string numeroDoDocumento, DateTime dataDeNascimento, string nomeDaRua, UInt32 numeroDaCasa)
+        {
+            this.nome = nome;
+            this.numeroDoDocumento = numeroDoDocumento;
+            this.dataDeNascimento = dataDeNascimento;
+            this.nomeDaRua = nomeDaRua;
+            this.numeroDaCasa = numeroDaCasa;
+        }
+    }
+}
